8ef38924f65520ca16ad549f6ce19ba0252fc75a
ilyzhin <lyzhin-ivan@yandex.ru>
2019-05-07 22:41:16 +0500

    simple model based on buzzwords

--- AdDetectorModel/model.py
+++ AdDetectorModel/model.py
@@ -1,4 +1,7 @@
-import time
+from AdDetectorModel.utils.scenes import SceneDetectionManager
+from AdDetectorModel.utils.subtitles import Subtitles
+from AdDetectorModel.utils.texts import preprocess_russian_text
+import nltk
 
 
 class AdDetectorModel:
@@ -11,9 +14,30 @@ class AdDetectorModel:
     def load(self, path):
         pass
 
-    def find_ads(self, video_id):
-        time.sleep(5)
-        return []
+    def find_ads(self, video_ids):
+        if not isinstance(video_ids, list):
+            video_ids = [video_ids]
+        result = []
+        buzz_words = ['ссылка', 'ссылочка', 'переходить', 'описание', 'партнер', 'скидка', 'купон',
+                      'распродажа', 'внизу', 'акция', 'рекомендуем', 'процент']
+        sno = nltk.stem.SnowballStemmer('russian')
+        buzz_words = list(map(sno.stem, buzz_words))
 
-    def train(self, video_ids, markups):
+        for video_id in video_ids:
+            subs = Subtitles(video_id, preprocess_russian_text)
+            sdm = SceneDetectionManager()
+            scenes = sdm.detect_scenes(video_id)
+            scene_weights = []
+            ads = []
+            for scene in scenes:
+                scene_subs = subs.fulltext(scene[0], scene[1])
+                scene_weight = len(list(filter(lambda term: term in buzz_words, scene_subs.split())))
+                if scene_weight > 3:
+                    ads.append(scene)
+                scene_weights.append(scene_weight)
+            result.append(ads)
+
+        return result if len(result) > 1 else result[0]
+
+    def train(self, markups):
         pass
--- AdDetectorModel/train_model.py
+++ AdDetectorModel/train_model.py
@@ -1,3 +1,4 @@
+import os
 import json
 from collections import defaultdict
 from argparse import ArgumentParser
@@ -6,23 +7,36 @@ from sklearn.model_selection import StratifiedKFold
 
 from AdDetectorModel.model import AdDetectorModel
 from AdDetectorModel.utils.evaluation import evaluate
+from AdDetectorModel.utils.youtube import YouTubeDownloader
+from AdDetectorUtils.paths import *
 
 
 def main():
     arg_parser = ArgumentParser()
     arg_parser.add_argument('--markups-file', type=str, help='path to file with markups')
-    arg_parser.add_argument('--cv-splits', type=int, default=3, help='number of splits in CV')
+    arg_parser.add_argument('--cv-splits', type=int, default=4, help='number of splits in CV')
     args = arg_parser.parse_args()
     if not args.markups_file:
         print('Please specify --markups-file')
         return
+    if not os.path.exists(args.markups_file):
+        print('Markups file does not exists.', args.markups_file)
+        return
     with open(args.markups_file, 'r') as mf:
         markups = json.load(mf)
     video_ids = list(markups.keys())
+    ytdl = YouTubeDownloader()
+    for i, video_id in enumerate(video_ids):
+        print('Preparing video {} ({}/{})'.format(video_id, i+1, len(video_ids)))
+        ytdl.load_all(video_id)
+    video_ids = list(filter(lambda video_id: subs_exists(video_id) and video_exists(video_id), video_ids))
+    print('Total videos: ', len(video_ids))
     has_ads = [len(markups[video_id]) > 0 for video_id in video_ids]
     cv = StratifiedKFold(n_splits=args.cv_splits, shuffle=True, random_state=0)
     avg_metrics = defaultdict(lambda: 0)
-    for train_idx, test_idx in cv.split(video_ids, has_ads):
+    for split_id, split in enumerate(cv.split(video_ids, has_ads)):
+        train_idx, test_idx = split
+        print('Cross-validation split #', split_id + 1)
         train_ids = [video_ids[idx] for idx in train_idx]
         test_ids = [video_ids[idx] for idx in test_idx]
         train_markups = {video_id: markups[video_id] for video_id in train_ids}
@@ -32,6 +46,7 @@ def main():
         metrics = evaluate(model, test_markups)
         for metric, value in metrics.items():
             avg_metrics[metric] += value
+        print(avg_metrics)
     for metric in avg_metrics.keys():
         avg_metrics[metric] /= args.cv_splits
         print('{}: {}'.format(metric, avg_metrics[metric]))

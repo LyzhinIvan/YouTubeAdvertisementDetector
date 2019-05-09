206e3735b2a4a9ddd0e36333597604168efca686
ilyzhin <lyzhin-ivan@yandex.ru>
2019-05-09 15:37:59 +0500

    info path

--- AdDetectorUtils/paths.py
+++ AdDetectorUtils/paths.py
@@ -18,6 +18,10 @@ def get_subs_path(video_id, lang='ru'):
     return join(config.get('paths', 'YouTubeData'), video_id, video_id + '.' + lang + '.vtt')
 
 
+def get_info_path(video_id):
+    return join(config.get('paths', 'YouTubeData'), video_id, video_id + '.info.json')
+
+
 def video_exists(video_id):
     return exists(get_video_path(video_id))
 

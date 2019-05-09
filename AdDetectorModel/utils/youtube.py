5c86424a215986d1e8ce6d9cde674345b4170694
ilyzhin <lyzhin-ivan@yandex.ru>
2019-05-09 15:36:25 +0500

    fix duplicates in subtitles

--- AdDetectorModel/utils/youtube.py
+++ AdDetectorModel/utils/youtube.py
@@ -2,16 +2,14 @@ import os
 import json
 import youtube_dl
 import ffmpeg
+from pycaption import DFXPReader, WebVTTWriter
 
 from AdDetectorUtils.paths import *
 
 
 class YouTubeDownloader:
-    def _get_info_path(self, video_id):
-        return join(get_dir(video_id), video_id + '.info.json')
-
     def load_info(self, video_id):
-        if not os.path.exists(self._get_info_path(video_id)):
+        if not os.path.exists(get_info_path(video_id)):
             print('Loading info for {}'.format(video_id))
             opts = {
                 'skip_download': True,
@@ -20,7 +18,7 @@ class YouTubeDownloader:
             }
             with youtube_dl.YoutubeDL(opts) as ytdl:
                 ytdl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
-        with open(self._get_info_path(video_id), 'r') as f:
+        with open(get_info_path(video_id), 'r') as f:
             return json.load(f)
 
     def load_video(self, video_id):
@@ -52,15 +50,36 @@ class YouTubeDownloader:
             opts = {
                 'writeautomaticsub': True,
                 'subtitleslangs': langs,
-                'subtitlesformat': 'vtt',
+                'subtitlesformat': 'ttml',
                 'nooverwrites': True,
                 'skip_download': True,
-                'outtmpl': join(get_dir(video_id), video_id + '.vtt')
+                'outtmpl': join(get_dir(video_id), video_id + '.ttml')
             }
             with youtube_dl.YoutubeDL(opts) as ytdl:
                 ytdl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
+            # WevVTT captions from youtube contains duplicate phrases with overlapping time segments
+            # It is not comfortable, that's why subtitles firstly downloaded in ttml format
+            # Then subtitles converted to webvtt
+            subs_path_ttml = join(get_dir(video_id), video_id + '.' + lang + '.ttml')
+            subs_path_vtt = join(get_dir(video_id), video_id + '.' + lang + '.vtt')
+            if exists(subs_path_ttml):
+                print('converting subtitles')
+                with open(subs_path_ttml, encoding='utf-8') as f:
+                    subs = DFXPReader().read(f.read())
+                with open(subs_path_vtt, 'w', encoding='utf-8') as f:
+                    f.write(WebVTTWriter().write(subs))
 
     def load_all(self, video_id):
         self.load_subtitles(video_id)
         self.load_video(video_id)
         self.extract_audio(video_id)
+
+
+class VideoInfo:
+    def __init__(self, video_id):
+        if not exists(get_info_path(video_id)):
+            ytdl = YouTubeDownloader()
+            ytdl.load_info(video_id)
+        with open(get_info_path(video_id)) as f:
+            self.info = json.load(f)
+        self.duration = self.info['duration']
